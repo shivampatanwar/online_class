2d581bb470c0298dcad2f0a8a699f8f84ad3d45b
Shivam Patanwar <shivampatanwarofficial@gmail.com>
2023-12-07 23:16:15 +0530

    first commit

---
+++ Sample.java
@@ -0,0 +1,18 @@
+class Sample
+{
+    public static void main(String[] args)
+    {
+        // int a = 123;
+        // System.out.println(a%10);
+        // System.out.println(a%100);
+        // System.out.println(a%1000);
+
+
+        int i = 8594;
+        System.out.println((i/100)%10);
+        
+
+
+
+    }
+}
