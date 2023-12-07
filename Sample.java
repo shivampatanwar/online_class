affd4accc764c90f2c71a7374e5403fe978f16cd
Shivam Patanwar <shivampatanwarofficial@gmail.com>
2023-12-07 23:34:52 +0530

    commit

--- Sample.java
+++ Sample.java
@@ -8,8 +8,22 @@ class Sample
         // System.out.println(a%1000);
 
 
-        int i = 8594;
-        System.out.println((i/100)%10);
+        // int i = 8594;
+        // System.out.println((i/100)%10);
+
+
+        // int j = 8594296;
+        // System.out.println(j/1000000);
+
+
+        int k = 7894;
+        System.out.println(k/10%10);
+        System.out.println((k/10)%10);
+
+
+
+
+
         
 
 
